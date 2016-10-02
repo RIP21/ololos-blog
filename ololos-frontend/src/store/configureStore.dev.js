3273bb8904df15224623440d183dc599110efab4
Andrii Los <puha212@gmail.com>
2016-10-03 00:43:30 +0200

    promiseMiddleware added, need to rewrite all actions/reducers so they all support it.

--- ololos-frontend/src/store/configureStore.dev.js
+++ ololos-frontend/src/store/configureStore.dev.js
@@ -2,10 +2,11 @@ import {createStore, compose, applyMiddleware} from 'redux';
 import rootReducer from '../reducers';
 import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
 import thunk from 'redux-thunk';
+import promiseMiddleware from 'redux-promise-middleware';
 
 export default function configureStore(initialState) {
   return createStore(rootReducer, initialState, compose(
-    applyMiddleware(thunk, reduxImmutableStateInvariant()),
+    applyMiddleware(thunk, promiseMiddleware(), reduxImmutableStateInvariant()),
     window.devToolsExtension ? window.devToolsExtension() : f => f // add support for Redux dev tools
     )
   );
--- ololos-frontend/src/store/configureStore.prod.js
+++ ololos-frontend/src/store/configureStore.prod.js
@@ -1,11 +1,12 @@
 import {createStore, applyMiddleware} from 'redux';
 import rootReducer from '../reducers';
 import thunk from 'redux-thunk';
+import promiseMiddleware from 'redux-promise-middleware';
 
 export default function configureStore(initialState) {
   return createStore(
     rootReducer,
     initialState,
-    applyMiddleware(thunk)
+    applyMiddleware(thunk, promiseMiddleware())
   );
 }
