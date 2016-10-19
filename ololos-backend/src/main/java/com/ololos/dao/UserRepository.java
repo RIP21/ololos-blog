9e090603124c31297e4d9ce6fb7c026806000e4f
RIP21 <puha212@gmail.com>
2016-10-19 19:38:14 +0200

    REST Controllers added. DAO Repositories added to replace manual query building.

--- ololos-backend/src/main/java/com/ololos/dao/UserRepository.java
+++ ololos-backend/src/main/java/com/ololos/dao/UserRepository.java
@@ -2,6 +2,8 @@ package com.ololos.dao;
 
 import com.ololos.domain.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(exported = false)
 public interface UserRepository extends MongoRepository<User, String> {
 }
