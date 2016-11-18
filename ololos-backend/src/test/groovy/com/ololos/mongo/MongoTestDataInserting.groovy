036b9f0dcf5d03bc87171fc7c98a7b0d7ddc1097
RIP21 <puha212@gmail.com>
2016-11-18 18:49:08 +0100

    - First commit to change to serverside rendering

--- ololos-backend/src/test/groovy/com/ololos/mongo/MongoTestDataInserting.groovy
+++ ololos-backend/src/test/groovy/com/ololos/mongo/MongoTestDataInserting.groovy
@@ -9,9 +9,6 @@ import com.ololos.domain.Post
 import com.ololos.domain.User
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.security.core.authority.SimpleGrantedAuthority
-/**
- * Created by Andrii_Los on 10/17/2016.
- */
 
 class MongoTestDataInserting extends AbstractMvcSpec {
 
@@ -34,8 +31,8 @@ class MongoTestDataInserting extends AbstractMvcSpec {
             Author second = new Author("LINA", "Lina Oleynik")
             authorRepository.save(first)
             authorRepository.save(second)
-            postRepository.save(new Post("title-1","title 1", "# Somebody post 1", new Date(), first))
-            postRepository.save(new Post("title-2","title 2", "# Somebody post 2", new Date(), second))
+            postRepository.save(new Post("title-1", "title 1", "# Somebody post 1", "", "", new Date(), first))
+            postRepository.save(new Post("title-2", "title 2", "# Somebody post 2", "", "", new Date(), second))
 
         then:
             userRepository.findAll().size() == 2;
